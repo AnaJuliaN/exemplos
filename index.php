4df2f156e369eed3d25ce361848d73be4833d351
AnaJuliaN <ana.nunes@aluno.ifsp.edu.br>
2020-03-03 22:57:36 -0300

    Links para os exemplos

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>Pagina de link com meus exemplos</h1>
+    <a href='20200303/exemplo1'>Exemplo 1</a>
+    <a href='20200303/exemplo2'>Exemplo 2</a>
+    <a href='20200303/exemplo3'>Exemplo 3</a>
+</body>
+</html>
