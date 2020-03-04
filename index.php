474f93d532b4eaecbd46d835fec746bb658663e6
AnaJuliaN <ana.nunes@aluno.ifsp.edu.br>
2020-03-03 23:04:27 -0300

    Links para os exemplos

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
     <h1>Pagina de link com meus exemplos</h1>
     <a href='20200303/exemplo1.html'>Exemplo 1 |</a>
     <a href='20200303/exemplo2.html'>Exemplo 2 |</a>
-    <a href='20200303/exemplo3'>Exemplo 3</a>
+    <a href='20200303/exemplo3.html'>Exemplo 3</a>
 </body>
 </html>
